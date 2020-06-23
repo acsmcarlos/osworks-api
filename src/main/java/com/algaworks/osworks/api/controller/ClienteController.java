d47955eeb9f6763222fdddd69fadc9e902dc0635
Antonio Carlos da silva Moreira <antoniomoreira@FGFCONSULTORES.com.br>
2020-06-23 09:43:23 -0300

    aula 02 continuando

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,31 +1,23 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
-	@GetMapping("/clientes")
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping("/clientes")     // BUSCAR TODOSO OS CLIENTES
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Tonin");
-		cliente1.setTelefone("8888-8888");
-		cliente1.setEmail("email");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Carlin");
-		cliente2.setTelefone("1111-1111");
-		cliente2.setEmail("email2");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
 
 }
--- src/main/java/com/algaworks/osworks/domain/model/Cliente.java
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -1,12 +1,22 @@
 package com.algaworks.osworks.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@EqualsAndHashCode(exclude = {"nome", "email", "telefone"})
 public class Cliente {
 	
+	@Id   // define a chave primaria da entidade
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  //auto increment
 	private Long id;
 	private String nome;
 	private String email;
